3b7598d8d62e757e50e23225634bf7fa1333ed45
myeongju24 <72543116+myeongju24@users.noreply.github.com>
2020-10-13 19:29:27 +0900

    room_usersテーブルの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   
   validates :name, presence: true
 
-  has many :room_users
-  has many :rooms, through: :room_users
+  has_many :room_users
+  has_many :rooms, through: :room_users
   
 end
